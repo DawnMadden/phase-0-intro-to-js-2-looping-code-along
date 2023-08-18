2fcfb11179a3a9e225ac13e8f35b8ce3dbcffe80
Dawn Madden <coachdawnmadden@gmail.com>
2023-08-17 22:39:08 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// solution
+function writeCards(names,events) {
+    let newArray=[];
+    for (let i = 0; i < names.length; i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+}
+return newArray
+}
+
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+    console.log(countDown--);    
+    }
+    
+} 
